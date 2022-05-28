b7e0bc24d87e8d423abe3d3746c0e474a4f67e15
Israel Akinwumi <israelakin14@gmail.com>
2022-05-28 21:28:41 +0800

    Completed Features section for small and large devices

--- src/containers/features/features.jsx
+++ src/containers/features/features.jsx
@@ -1,8 +1,62 @@
 import React from 'react'
 import './features.css'
+import { RiArrowRightLine } from 'react-icons/ri'
+import { Feature } from '../../components'
 
+const featuresData = [
+  {
+    id: 0,
+    title: 'Experience Natuer firsthand and see the importance',
+    content:
+      'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Itaque aliquam nemo eligendi deleniti porro omnis esse dolorem, ex natus voluptatibus debitis in soluta ad perferendis iste, temporibus molestias, voluptate veritatis!',
+  },
+  {
+    id: 1,
+    title: 'Unforgetable experiecne with nature',
+    content:
+      'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Itaque aliquam nemo eligendi deleniti porro omnis esse dolorem, ex natus voluptatibus debitis in soluta ad perferendis iste, temporibus molestias, voluptate veritatis!',
+  },
+  {
+    id: 0,
+    title: 'Experience Natuer firsthand and see the importance',
+    content:
+      'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Itaque aliquam nemo eligendi deleniti porro omnis esse dolorem, ex natus voluptatibus debitis in soluta ad perferendis iste, temporibus molestias, voluptate veritatis!',
+  },
+  {
+    id: 1,
+    title: 'Unforgetable experiecne with nature',
+    content:
+      'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Itaque aliquam nemo eligendi deleniti porro omnis esse dolorem, ex natus voluptatibus debitis in soluta ad perferendis iste, temporibus molestias, voluptate veritatis!',
+  },
+]
 const features = () => {
-  return <div>features</div>
+  const Iconstyle = {
+    margin: '0 10px',
+    color: 'var(--color-subtext)',
+  }
+  return (
+    <div className="amill__feautures-section">
+      <div className="amill__feautures-intro">
+        <h1 className="gradient__text">
+          Explore multiple reasons to enjoy nature today
+        </h1>
+        <p className="gradient__text">
+          Request weekly
+          <RiArrowRightLine style={Iconstyle} />
+        </p>
+      </div>
+      <div className="amill__feautures-details">
+        {featuresData.map((data) => (
+          <Feature
+            title={data.title}
+            content={data.content}
+            key={data.id}
+            flexdirection={'row'}
+          />
+        ))}
+      </div>
+    </div>
+  )
 }
 
 export default features
